9d76d36e4b0aedeb0c6231578e9d62994b1c0f87
Xingcan Chen <xingcanchen2401@163.com>
2022-05-28 16:34:34 +0800

    按照例子完成冒泡排序，并测试通过

---
+++ src/BubbleSort.java
@@ -0,0 +1,26 @@
+public class BubbleSort {
+    public static int[] sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            boolean swap = false;
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                    swap = true;
+                }
+            }
+            if (!swap) break;
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+
+        int[] src = {8, 4, 6, 2, 5, 7, 3, 9, 1};
+        int[] dest = BubbleSort.sort(src);
+        for (int num : dest) {
+            System.out.print(num + "\t");
+        }
+    }
+}
